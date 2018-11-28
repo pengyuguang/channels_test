727346dfff95379da2845d7a9be6db6607b3248a
pyg <616157198@qq.com>
2018-11-29 05:11:15 +0800

    debug: 修复一些错误，代码勉强可以用了。

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,19 +6,21 @@ import json
 
 @channel_session
 def ws_connect(message):
-    prefix, label = message['path'].strip('/').split('/')
-    room = Room.objects.get(label=label)
+    path = str(message['path'], encoding="utf8")
+    prefix, label = path.strip('/').split('/')
+    room, created = Room.objects.get_or_create(label=label)
     Group('chat-' + label).add(message.reply_channel)
     message.channel_session['room'] = room.label
 
 
 @channel_session
 def ws_receive(message):
-    label=message.channel_session['room']
+    label = message.channel_session['room']
     room = Room.objects.get(label=label)
     data = json.loads(message['text'])
     m = room.messages.create(handle=data['handle'], message=data['message'])
-    Group('chat-'+label).send({'text': json.dumps(m.as_dict())})
+    Group('chat-'+label).send({'text': json.dumps({'handle': m.handle, 'message': m.message,
+                                                   'timestamp': m.timestamp.timestamp()})})
 
 
 @channel_session
